a9316ae96198d2afce3a3c91a9d329825bea6237
Olivia Mignone <om651@nyu.edu>
2016-02-25 15:55:35 -0500

    Added char controller & mouse look

---
+++ Assets/Scripts/CharControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharControl : MonoBehaviour {
+
+	CharacterController myController;
+	public float walkSpeed = 5f;
+	public float turnSpeed = 200f;
+
+	// Use this for initialization
+	void Start () {
+		myController = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = transform.forward * walkSpeed * vertical;
+		myController.Move ((movement + Physics.gravity * 2f) * Time.deltaTime);
+
+		transform.Rotate (0f, horizontal * turnSpeed * Time.deltaTime, 0f);
+	}
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float rotateSpeed = 150f;
+	float rotationY;
+
+	// Update is called once per frame
+	void Update () {
+		
+		//if we aren't using the mouse, these values are zero
+		float mouseX = Input.GetAxis ("Mouse X"); //move left = -1
+		float mouseY = Input.GetAxis ("Mouse Y"); //move down = -1
+
+		//causes inverted mouseY, camera rolling, and no clamping of Y axis
+		//transform.Rotate(mouseY * Time.deltaTime * rotateSpeed, mouseX * Time.deltaTime * rotateSpeed, 0f);
+
+		rotationY += -mouseY * Time.deltaTime * rotateSpeed;
+		rotationY = Mathf.Clamp (rotationY, -80f, 80f); //so we don't keep looking upside down, constrains movement
+
+		transform.Rotate (0f, mouseX * Time.deltaTime * rotateSpeed, 0f); //horizontal mouse move
+		transform.localEulerAngles = new Vector3 (rotationY, transform.localEulerAngles.y, 0f);  //vertical mouse move, setting Z to 0 unrolls camera
+
+		//hide cursor & lock it in center of screen
+		if (Input.GetMouseButtonDown (0)) {
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+}
